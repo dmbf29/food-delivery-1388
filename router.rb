9856b2c8c6f8026d935807c0fbd31523fb424055
Douglas Berkley <douglasmberkley@gmail.com>
2023-10-16 16:44:23 +0900

    added router logic

--- router.rb
+++ router.rb
@@ -1,1 +1,43 @@
-# TODO: implement the router of your app.
+class Router
+  def initialize(controller)
+    @controller = controller
+    @running    = true
+  end
+
+  def run
+    puts "Welcome to the Food Delivery!"
+    puts "           --           "
+
+    while @running
+      display_tasks
+      action = gets.chomp.to_i
+      print `clear`
+      route_action(action)
+    end
+  end
+
+  private
+
+  def route_action(action)
+    case action
+    # when 1 then @controller.list
+    when 9 then stop
+    else
+      puts "Please press 1, 2, 3 or 4"
+    end
+  end
+
+  def stop
+    @running = false
+  end
+
+  def display_tasks
+    puts ""
+    puts "What do you want to do next?"
+    puts "1 - List all meals"
+    puts "2 - Add a new meal"
+    puts "3 - List all customers"
+    puts "4 - Add a new customer"
+    puts "9 - Stop and exit the program"
+  end
+end
